77ef5a04fb6f05f5ef8ae24fd0ee42715c3194df
Braydon Sutherland <BraydonSutherland@gmail.com>
2022-09-09 07:56:16 -0500

    file setup first commit

---
+++ PhoneBookConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PhoneBookConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
