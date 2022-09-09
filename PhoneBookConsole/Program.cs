18ed97ace18c23aeef1e889f075870a295cd57d7
Braydon Sutherland <BraydonSutherland@gmail.com>
2022-09-09 08:43:52 -0500

    working on user input via a switch statment

--- PhoneBookConsole/Program.cs
+++ PhoneBookConsole/Program.cs
@@ -12,8 +12,31 @@ namespace PhoneBookConsole
             Console.WriteLine("2. Display Contact by Number");
             Console.WriteLine("3. View all Contacts");
             Console.WriteLine("4. Search for contacts for a given name");
+            Console.WriteLine(); 
+            Console.Write("Please Enter your Seleciton: "); 
+            var userInput = Console.ReadLine();
 
-            var userInput = Console.ReadLine(); 
+            var phoneBook = new PhoneBook();
+
+            switch (userInput)
+            {
+                case "1":
+                    Console.Write("Contact name: ");
+                    var name = Console.ReadLine();
+                    Console.Write("Contact numer: ");
+                    var number = Console.ReadLine(); 
+
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                default:
+                    Console.WriteLine("Select valid operation");
+                    break; 
+            }
         }
     }
 }
