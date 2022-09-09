b0edec9e2d8202b0895ac92d1abc25f17f9c780b
Braydon Sutherland <BraydonSutherland@gmail.com>
2022-09-09 08:28:45 -0500

    adding user input

--- PhoneBookConsole/Program.cs
+++ PhoneBookConsole/Program.cs
@@ -6,7 +6,14 @@ namespace PhoneBookConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello From the Console PhoneBook App!"); 
+            Console.WriteLine("Hello From the Console PhoneBook App!");
+            Console.WriteLine("Select operation: ");
+            Console.WriteLine("1. Add Contact");
+            Console.WriteLine("2. Display Contact by Number");
+            Console.WriteLine("3. View all Contacts");
+            Console.WriteLine("4. Search for contacts for a given name");
+
+            var userInput = Console.ReadLine(); 
         }
     }
 }
